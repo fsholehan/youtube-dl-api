a41142d966ebc62a232de1e69ff301ffa901d580
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-04-13 05:30:50 +0700

    add lyric

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const ytdl = require("ytdl-core");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const lyricsFinder = require("@jeve/lyrics-finder");
 
 const app = express();
 
@@ -11,8 +12,16 @@ app.get("/api/v1/music", async (req, res) => {
   const { videoId } = req.query;
   let info = await ytdl.getInfo(videoId);
   let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-  console.log("Formats with only audio: " + audioFormats);
+  // console.log(info);
+  // console.log("Formats with only audio: " + audioFormats);
   res.json(audioFormats);
+});
+
+app.get("/api/v1/lyric", async (req, res) => {
+  const { title } = req.query;
+  await lyricsFinder.LyricsFinder(title).then((data) => {
+    res.json(data);
+  });
 });
 
 app.listen(port, () => console.log(`Server running on, ${port}`));
