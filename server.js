c65734019a81a85a34e5d4f5bc0fb0eb4c90640f
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-04-13 08:23:20 +0700

    Add all api

--- server.js
+++ server.js
@@ -1,29 +1,70 @@
 const ytdl = require("ytdl-core");
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
 const lyricsFinder = require("@jeve/lyrics-finder");
+const { getChart } = require("billboard-top-100");
+const ytsr = require("ytsr");
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 
+//search data
+app.get("/api/v1/music/search", async (req, res) => {
+  const { keyword } = req.query;
+  try {
+    const searchResults = await ytsr(keyword);
+    res.json(searchResults.items);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// video to audio
 app.get("/api/v1/music", async (req, res) => {
   const { videoId } = req.query;
   let info = await ytdl.getInfo(videoId);
   let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-  // console.log(info);
   // console.log("Formats with only audio: " + audioFormats);
   res.json(audioFormats);
 });
 
+//related videos
+app.get("/api/v1/music/related", async (req, res) => {
+  const { videoId } = req.query;
+  let info = await ytdl.getInfo(videoId);
+  let related_music = info.related_videos;
+  console.log("realted videos: ", related_music);
+  res.json(related_music);
+});
+
+//details video
+app.get("/api/v1/music/details", async (req, res) => {
+  const { videoId } = req.query;
+  let info = await ytdl.getInfo(videoId);
+  let details = info.videoDetails;
+  console.log("realted videos: ", details);
+  res.json(details);
+});
+
+//get lyrics
 app.get("/api/v1/lyric", async (req, res) => {
   const { title } = req.query;
   await lyricsFinder.LyricsFinder(title).then((data) => {
     const result = {
-      data
-    }
+      data,
+    };
     res.json(result);
+  });
+});
+
+//list billboard
+app.get("/api/v1/billboard", async (req, res) => {
+  getChart((err, chart) => {
+    if (err) console.log(err);
+    res.json(chart);
   });
 });
 
